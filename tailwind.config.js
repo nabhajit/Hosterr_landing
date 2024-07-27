be407da0abf65dd054359196ab7e97f4d498896a
Coding_variable <roynabhajit@gmail.com>
2024-07-27 13:35:26 +0530

    hosterr

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: "'Playfair Display', serif",
+        lato: "'Lato', sans-4serif",
+      }
+    },
+  },
+  plugins: [],
+}
+
